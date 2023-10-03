869dd51bcec68fa7342c67c7c3d6fb56606a2a61
GotzonViteri <GotzonViteri@DESKTOP-OL24BGQ>
2023-10-03 22:08:09 +0200

    xd

--- Programacion3Practicas/src/Practica06VentanaAleatoria/VentanaPrincipal.java
+++ Programacion3Practicas/src/Practica06VentanaAleatoria/VentanaPrincipal.java
@@ -1,5 +1,28 @@
 package Practica06VentanaAleatoria;
 
-public class VentanaPrincipal {
 
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+/** Código de base para una ventana de Swing, de cara a completar el ejercicio 0.6
+ * @author andoni.eguiluz @ ingenieria.deusto.es
+ */
+@SuppressWarnings("serial")
+public class VentanaPrincipal {
+	private static Dimension TAMANYO_INICIAL_VENTANA = new Dimension( 400, 300 );
+
+	public static void main(String[] args) {
+		VentanaPrincipal v = new VentanaPrincipal();
+		v.setVisible(true);
+	}
+
+	public VentanaPrincipal() {
+		super( "Ventana aleatoria - ej. 0.6" );
+		setSize( TAMANYO_INICIAL_VENTANA );
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+	}
+
+	
 }
